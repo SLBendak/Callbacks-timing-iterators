0cd2730c10dead33745a4bdeeca1e3db57bfba87
Shane Bendak <shebah712@gmail.com>
2020-07-16 15:32:12 -0700

    [callback] calculator

--- main.js
+++ main.js
@@ -16,3 +16,53 @@ function anotherGreeting() {
 
 let myName = 'Shane';
 console.log(greeting(myName, anotherGreeting));
+
+
+// Make a function
+
+// function printArray(array, callback){
+//     for (let i = 0; i < array.length; i++){
+//         let numbers = array[i];
+
+//         console.log(numbers);
+//     }
+//     callback(array);
+
+// }
+
+// function printArrayAgain(array){
+//     for (let i = 0; i < array.length; i++){
+//         let number = array[i];
+
+//         console.log(number)
+//     }
+
+// }
+
+// const numbersArray = [1, 2, 3, 4, 5, 6];
+
+// printArray(numbersArray, printArray);
+
+// // exercise
+
+function calculator(num1, num2, callbackOne, callbackTwo){
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+
+    return result;
+}
+
+function addNumbers(num1, num2){
+    let result = num1 + num2;
+
+    return result;
+
+}
+
+function subtractNumbers(num1, num2){
+    let result = num1 - num2;
+
+    return result;
+
+}
+
+console.log(calculator(5,77, addNumbers, subtractNumbers));
